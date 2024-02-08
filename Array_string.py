389eed531c917abde2bc54fcb3efaceba7bf91fa
HyunkiKim0407 <ralgusrl47@gamil.com>
2024-02-08 23:31:20 +0900

    commit on "main"

---
+++ Array_string.py
@@ -0,0 +1,12 @@
+# def solution(strlist):
+#     answer = []
+#     for i in range(len(strlist)):
+#         answer = len(strlist[i])
+#     return answer
+# solution(["We", "are", "the", "world!"])]
+answer = []
+a = ["We", "are", "the", "world!"]
+for i in range(len(a)):
+    b = (len(a[i]))
+    answer.append(b)
+    print(answer)
