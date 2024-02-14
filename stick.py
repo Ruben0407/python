9311f97df5a3adc4a7e642175848c5b9aae48595
HyunkiKim0407 <ralgusrl47@gamil.com>
2024-02-14 22:53:41 +0900

    commit on "main"

--- stick.py
+++ stick.py
@@ -2,9 +2,9 @@ def solution(n, k):
     answer = 0
     stick = 12000
     juice = 2000
-    i = 0
-    k = 0
     for i in range(n):
         if n // 10 == 1 * i:
             k -= 1 * i
             answer = (stick * n) + (juice * k)
+            print(answer)
+solution(10, 3)
