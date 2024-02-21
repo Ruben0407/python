c186d1bfc0a40387d32ed8e5e94befdf1ab99b4f
HyunkiKim0407 <ralgusrl47@gamil.com>
2024-02-21 12:50:27 +0900

    commit on main

--- Cloth_store.py
+++ Cloth_store.py
@@ -1,11 +1,11 @@
 def solution(price):
     answer = 0
     if 300000 > price >= 100000:
-        answer = price - int(price * 0.05)
+        answer = int(price * 0.95)#0.05를 빼서 구하지말고 바로 구하면 풀이가 완성
     elif 500000 > price >= 300000:
-        answer = price - int(price * 0.1)
+        answer = int(price * 0.9)#마찬가지
     elif price >= 500000:
-        answer = price - int(price * 0.2)
+        answer = int(price * 0.8)
     else:
         answer = price
     print(answer)
